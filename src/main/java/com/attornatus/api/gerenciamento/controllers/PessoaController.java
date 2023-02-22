1d6a969c119f256a387f7ebba819a959e4f26ce9
José Roberto Jr <joserobertodsj@gmail.com>
2023-02-21 22:32:50 -0300

    refactor: change dependency injection

--- src/main/java/com/attornatus/api/gerenciamento/controllers/PessoaController.java
+++ src/main/java/com/attornatus/api/gerenciamento/controllers/PessoaController.java
@@ -3,6 +3,7 @@ package com.attornatus.api.gerenciamento.controllers;
 import com.attornatus.api.gerenciamento.models.dtos.requests.PessoaRequestDto;
 import com.attornatus.api.gerenciamento.models.dtos.responses.PessoaResponseDto;
 import com.attornatus.api.gerenciamento.services.impl.PessoaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ public class PessoaController {
 
     private PessoaServiceImpl pessoaService;
 
+    @Autowired
     public PessoaController(PessoaServiceImpl pessoaService) {
         this.pessoaService = pessoaService;
     }
