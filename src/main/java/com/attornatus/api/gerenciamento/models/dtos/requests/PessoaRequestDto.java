42dceee31e7864b2da2d3d632cb9b387c3f5c689
José Roberto Jr <joserobertodsj@gmail.com>
2023-02-21 22:30:28 -0300

    fix: change record for class in Pessoa dtos

--- src/main/java/com/attornatus/api/gerenciamento/models/dtos/requests/PessoaRequestDto.java
+++ src/main/java/com/attornatus/api/gerenciamento/models/dtos/requests/PessoaRequestDto.java
@@ -1,15 +1,16 @@
 package com.attornatus.api.gerenciamento.models.dtos.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PessoaRequestDto{
 
-public record PessoaRequestDto(
-
-        String nome,
-        LocalDate dataDeAniversario
-
-
-) {
-
-
-
+        private String nome;
+        private LocalDate dataDeAniversario;
 }
--- src/main/java/com/attornatus/api/gerenciamento/models/dtos/responses/PessoaResponseDto.java
+++ src/main/java/com/attornatus/api/gerenciamento/models/dtos/responses/PessoaResponseDto.java
@@ -1,17 +1,19 @@
 package com.attornatus.api.gerenciamento.models.dtos.responses;
 
 import com.attornatus.api.gerenciamento.models.Endereco;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
 import java.util.List;
-
-public record PessoaResponseDto(
-        Long idPessoa,
-        String nome,
-        LocalDate dataDeAniversario,
-        List<Endereco> enderecos
-
-
-
-) {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PessoaResponseDto{
+        private Long idPessoa;
+        private String nome;
+        private LocalDate dataDeAniversario;
+        private List<Endereco> endereco;
 }
