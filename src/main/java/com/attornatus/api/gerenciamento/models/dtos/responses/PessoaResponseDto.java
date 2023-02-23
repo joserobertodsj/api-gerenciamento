a5bbe87ae3f5fd04bc4759afee1f9321fa1bfcd7
José Roberto Jr <joserobertodsj@gmail.com>
2023-02-23 14:23:28 -0300

    refactor: change list type in PersonResponseDto

--- src/main/java/com/attornatus/api/gerenciamento/models/dtos/responses/PessoaResponseDto.java
+++ src/main/java/com/attornatus/api/gerenciamento/models/dtos/responses/PessoaResponseDto.java
@@ -1,10 +1,10 @@
 package com.attornatus.api.gerenciamento.models.dtos.responses;
 
-import com.attornatus.api.gerenciamento.models.Endereco;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.time.LocalDate;
 import java.util.List;
 @AllArgsConstructor
@@ -15,5 +15,5 @@ public class PessoaResponseDto{
         private Long idPessoa;
         private String nome;
         private LocalDate dataDeAniversario;
-        private List<Endereco> endereco;
+        private List<EnderecoResponseDto> enderecos;
 }
