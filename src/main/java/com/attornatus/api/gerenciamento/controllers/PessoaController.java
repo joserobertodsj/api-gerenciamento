6d87e90ae70148acfce3936e5b60037b38d4ec56
José Roberto Jr <joserobertodsj@gmail.com>
2023-02-23 15:06:20 -0300

    feat: create updatePerson in PersonController

--- src/main/java/com/attornatus/api/gerenciamento/controllers/PessoaController.java
+++ src/main/java/com/attornatus/api/gerenciamento/controllers/PessoaController.java
@@ -4,6 +4,8 @@ import com.attornatus.api.gerenciamento.models.dtos.requests.PessoaRequestDto;
 import com.attornatus.api.gerenciamento.models.dtos.responses.PessoaResponseDto;
 import com.attornatus.api.gerenciamento.services.impl.PessoaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +31,17 @@ public class PessoaController {
     @GetMapping("/{id}")
     public ResponseEntity<PessoaResponseDto> buscarPorId(@PathVariable(value = "id") Long idPessoa){
         return ResponseEntity.status(HttpStatus.OK).body(pessoaService.buscarPorId(idPessoa));
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PessoaResponseDto>> buscarTodos(Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(pessoaService.buscarTodos(pageable));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PessoaResponseDto> atualizarPessoa(@PathVariable(value = "id") Long id, @RequestBody PessoaRequestDto pessoaRequestDto){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(pessoaService.atualizar(id, pessoaRequestDto));
 
     }
 }
