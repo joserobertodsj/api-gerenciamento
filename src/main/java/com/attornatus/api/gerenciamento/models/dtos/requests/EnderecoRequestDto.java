900961a84cc43a009f26fccdaab05f0daeb2071a
José Roberto Jr <joserobertodsj@gmail.com>
2023-02-24 10:55:07 -0300

    feat: add annotation in EnderecoRequestDto

--- src/main/java/com/attornatus/api/gerenciamento/models/dtos/requests/EnderecoRequestDto.java
+++ src/main/java/com/attornatus/api/gerenciamento/models/dtos/requests/EnderecoRequestDto.java
@@ -1,5 +1,6 @@
 package com.attornatus.api.gerenciamento.models.dtos.requests;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,6 @@ public class EnderecoRequestDto{
         private String numero;
         private String cidade;
         private Boolean enderecoPrincipal;
+        @JsonIgnore
         private Long idPessoa;
 }
