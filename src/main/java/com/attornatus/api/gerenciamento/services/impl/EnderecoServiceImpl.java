ab6aa58570f8c60e6b732ef7bf1a0250ff85d904
José Roberto Jr <joserobertodsj@gmail.com>
2023-02-24 11:01:41 -0300

    feat: implement methods in EnderecoServiceImpl

--- src/main/java/com/attornatus/api/gerenciamento/services/impl/EnderecoServiceImpl.java
+++ src/main/java/com/attornatus/api/gerenciamento/services/impl/EnderecoServiceImpl.java
@@ -1,10 +1,14 @@
 package com.attornatus.api.gerenciamento.services.impl;
 
+import com.attornatus.api.gerenciamento.exceptions.ModelException;
+import com.attornatus.api.gerenciamento.models.Endereco;
+import com.attornatus.api.gerenciamento.models.Pessoa;
 import com.attornatus.api.gerenciamento.models.dtos.requests.EnderecoRequestDto;
 import com.attornatus.api.gerenciamento.models.dtos.responses.EnderecoResponseDto;
 import com.attornatus.api.gerenciamento.repositories.EnderecoRepository;
 import com.attornatus.api.gerenciamento.services.EnderecoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +17,43 @@ public class EnderecoServiceImpl implements EnderecoService {
 
 
     private final EnderecoRepository enderecoRepository;
+    private final PessoaServiceImpl pessoaService;
+
+
+    private final ModelMapper modelMapper;
 
 
     //------------------------------------------------------------------------------------------------------------------
 
-    @Override
-    public EnderecoResponseDto salvarEndereco(EnderecoRequestDto enderecoRequestDto) {
-        return null;
+    public EnderecoResponseDto buscarPorId(Long idEndereco){
+        return modelMapper.map(enderecoRepository.findById(idEndereco).orElseThrow(ModelException::new), EnderecoResponseDto.class);
     }
+
+    @Override
+    public EnderecoResponseDto salvarEndereco(Long idPessoa, EnderecoRequestDto enderecoRequestDto) {
+
+        Pessoa pessoa = modelMapper.map(pessoaService.buscarPorId(idPessoa), Pessoa.class);
+        var endereco = modelMapper.map(enderecoRequestDto, Endereco.class);
+        endereco.setEnderecoPrincipal(pessoa.getEnderecos().isEmpty());
+        endereco.setPessoa(pessoa);
+        enderecoRepository.save(endereco);
+        return modelMapper.map(endereco, EnderecoResponseDto.class);
+    }
+
+
+    public Endereco atualizarEnderecoPrincipal(Long idEndereco){
+        var endereco = modelMapper.map(enderecoRepository.findById(idEndereco), Endereco.class);
+
+        endereco.getPessoa().getEnderecos().forEach(e ->{
+            e.setEnderecoPrincipal(false);
+            enderecoRepository.save(e);
+        });
+        endereco.setEnderecoPrincipal(true);
+
+        return enderecoRepository.save(endereco);
+    }
+
+
+
+
 }
